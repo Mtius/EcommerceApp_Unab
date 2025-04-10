ccd4d8ac20aeb9306410ebc2b1b1a10a5e1e0947
Santiago Matheus <smatheus@unab.edu.co>
2025-04-10 15:56:50 -0500

    Primer avance del homeScreen

---
+++ app/src/main/java/co/edu/unab/ecommerceapp/santiagomatheus/HomeScreen.kt
@@ -0,0 +1,26 @@
+package co.edu.unab.ecommerceapp.santiagomatheus
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+
+
+@Composable
+fun HomeScreen(){
+    Scaffold { innerPadding ->
+        Column (modifier = Modifier.padding(innerPadding)){
+            Text(
+                "HOME SCREEN"
+                fontSize = 50.sp
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview(){
+
+}
